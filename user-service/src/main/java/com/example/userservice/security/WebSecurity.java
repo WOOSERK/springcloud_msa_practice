502bcf0d47ba6375a013a6319860284dfdead278
WOOSERK <syeokim@naver.com>
2022-04-11 18:52:20 +0900

    ch7. RabbitMQ end

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -38,6 +38,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter
     {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
             .access("hasIpAddress('" + "127.0.0.1" + "')")
             .and()
